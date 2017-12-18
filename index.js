f264f96bf23b6c04b4d34ebced3a4e4500056fbf
Nicole Creshon <ncreshon@gmail.com>
2017-12-18 15:06:06 -0600

    adds static files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4500;
 const app = express();
 
-// app.use()
+app.use(express.static(path.join(__dirname, '/client')));
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-app.get('/', (req, res) => {
-  res.json('hey');
-});
+// app.get('/', (req, res) => {
+//   res.json('hey');
+// });
