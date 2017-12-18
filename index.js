9fb831af67124a24023c1e07486195be2dcea025
Nicole Creshon <ncreshon@gmail.com>
2017-12-18 15:01:49 -0600

    adds to Procfile

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const PORT = process.env.PORT;
+const dotenv = require('dotenv');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-
+// app.use()
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 app.get('/', (req, res) => {
   res.json('hey');
