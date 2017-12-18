cf6fda7d2c604b499ca03de356d4a4e7aff6025f
Nicole Creshon <ncreshon@gmail.com>
2017-12-18 15:28:22 -0600

    updates port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const PORT = process.env.PORT || 4500;
+const Port = process.env.PORT || 4500;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`listening on ${Port}`));
 // app.get('/', (req, res) => {
 //   res.json('hey');
 // });
