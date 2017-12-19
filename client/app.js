4929446e1e3319534fc8782ec79823f2b23dd908
Nicole Creshon <ncreshon@gmail.com>
2017-12-19 12:46:30 -0600

    video player dynamically renders

--- client/app.js
+++ client/app.js
@@ -14,20 +14,19 @@ App.factory('Search', $http => ({
     headers: { search: query },
   }),
 }));
-App.controller('AppCtrl', ($scope, Search) => {
-  $scope.styletext;
+App.controller('AppCtrl', ($scope, $sce, Search) => {
+  
   $scope.producttext;
-  $scope.video;
+  $scope.video = { id: { videoId: 'N8XCXdmM9nM'}};
+  $scope.test = 'http://www.youtube.com/embed/';
+  $scope.trustSrc = function (src) {
+    return $sce.trustAsResourceUrl(src);
+  }
   $scope.styleVideos = [];
   $scope.productVideos = [];
-  $scope.theBestVideo = 'sMKoNBRZM1M';
-  $scope.youtubeUrl = (id) => {
-    return "https://www.youtube.com/embed/" + id;
-  };
   $scope.productsubmit = () => {
     Search.productSearch($scope.producttext).then((videos) => {
       $scope.productVideos = videos.data.items;
-      console.log($scope.productVideos);
     }).catch((err) => {
       console.log(err);
     });
@@ -36,8 +35,6 @@ App.controller('AppCtrl', ($scope, Search) => {
     Search.styleSearch($scope.styletext).then((videos) => {
       $scope.styleVideos = videos.data.items;
       $scope.video = videos.data.items[0];
-      console.log($scope.video, 'video')
-      console.log($scope.styleVideos)
     })
       .catch((err) => {
         console.log(err);
@@ -45,8 +42,8 @@ App.controller('AppCtrl', ($scope, Search) => {
   };
   $scope.titleSelect = (video) => {
     $scope.video = video;
-   console.log($scope.video)
   };
+  
 
   $scope.favorite = (video) => {
     console.log(video);
